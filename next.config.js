56ff7ceb69d412991afc10f15a77580037da3bb2
Navpreet singh <navpreetsingh41548@gmail.com>
2023-01-16 07:59:37 -0800

    Updated Url

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     MAIL_USERNAME: "navwebdev2@gmail.com",
     MAIL_PASSWORD: "gzlduanoellhlssa",
     // BASE_URL: "http://localhost:3000",
-    BASE_URL: "https://chakra-kriya-light.vercel.app",
+    BASE_URL: "https://chakrakriya.org",
     MongoDb:
       "mongodb+srv://user:Kj3KQ3oqNIDKzBS6@cluster0.6tfyvrn.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "jkahsdfkljasdklfjkl",
